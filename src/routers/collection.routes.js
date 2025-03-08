49ed49112993ae6bcb039eab40ab46736d649430
gi4n9 <giangnguyenlamtruong@gmail.com>
2025-03-08 15:24:22 +0700

    dẫn lại routes collection, chỉnh sửa css homepage

--- src/routers/collection.routes.js
+++ src/routers/collection.routes.js
@@ -1,8 +1,26 @@
 const express = require("express");
 const router = express.Router();
 
+const products = [
+  {
+    id: "ao-thun-nam",
+    name: "Áo Thun Nam Việt Devils - Marcus Rashford",
+    price: "330.000đ",
+    oldPrice: "389.000đ",
+    discount: "-15%",
+    image: "/img/localisz2.webp",
+  },
+];
+
 router.get("/", (req, res) => {
   res.render("collection", { title: "Bộ sưu tập" });
+});
+
+router.get("/:id", (req, res) => {
+  res.render("collection-id", {
+    title: "Bộ sưu tập",
+    products: products,
+  });
 });
 
 module.exports = router;
--- src/routers/product.routes.js
+++ src/routers/product.routes.js
@@ -1,29 +1,30 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // Dữ liệu mẫu sản phẩm
 const product = {
-    id: 1,
-    name: "Áo Thun Nam Việt Devils - Marcus Rashford",
-    oldPrice: "389.000đ",
-    price: "330.000đ",
-    discount: "-15%",
-    description: "Vải Cotton Compact",
-    shipping: "Miễn phí - Giao chậm 1-2 ngày",
-    discount: 15,
-    images: [ // Nhiều ảnh chính
-        "/img/localisz2.webp",
-        "/img/_CMM8493_24.webp"
-    ],
-    colors: ["#ffffff", "#000000", "#ff5733"],
-    sizes: ["S", "M", "L", "XL", "2XL", "3XL"]
+  id: 1,
+  name: "Áo Thun Nam Việt Devils - Marcus Rashford",
+  oldPrice: "389.000đ",
+  price: "330.000đ",
+  discount: "-15%",
+  description: "Vải Cotton Compact",
+  shipping: "Miễn phí - Giao chậm 1-2 ngày",
+  discount: 15,
+  images: [
+    // Nhiều ảnh chính
+    "/img/localisz2.webp",
+    "/img/_CMM8493_24.webp",
+  ],
+  colors: ["#ffffff", "#000000", "#ff5733"],
+  sizes: ["S", "M", "L", "XL", "2XL", "3XL"],
 };
 
 router.get("/:id", (req, res) => {
-    res.render("product", {
-        title: "Sản Phẩm",
-        product: product
-    });
+  res.render("product", {
+    title: "Sản Phẩm",
+    product: product,
+  });
 });
 
 module.exports = router;
