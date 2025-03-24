f0067da2dda656807bbbe707b2b77eb5c43202b3
NguyenNam <hainam13122004@gmail.com>
2025-03-24 20:34:09 +0700

    UI cứng, chưa đổ data của user

---
+++ src/routers/userprofile.routes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+// Sửa từ '/userprofile' thành '/'
+router.get('/', (req, res) => {
+    res.render('userprofile', {
+        // Dữ liệu động sẽ được truyền vào đây sau
+    });
+});
+
+module.exports = router;
